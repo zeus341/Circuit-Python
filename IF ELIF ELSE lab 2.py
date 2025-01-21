42650f0c77f6147bf2cca2db0b19592207c7ce55
zeus341 <bryan.jparra00@gmail.com>
2025-01-21 10:14:05 -0800

    IF ELIF ELSE lab 2.py

---
+++ IF ELIF ELSE lab 2.py
@@ -0,0 +1,46 @@
+# Comparing Variables and Objects - Lab 2
+# Last Edit Jan 30 2023
+print('''
+Compare Variables or Objects For Equivalence:
+
+This Lab is about Asking Questions and comparing Variables 
+or Objects.  Is the Answer "TRUE"?
+      
+The == is a comparison to see if 2 variables or objects 
+are equivalent or equal, but there are many other ways 
+to compare objects
+      
+x == y      # x is equal to y
+x != y      # x is not equal to y
+x > y       # x is greater than y
+x < y       # x is less than y
+x >= y      # x is greater than or equal to y
+x <= y      # x is less than or equal to y
+x is y      # x is the same as y
+x is not y  # x is not the same as y
+      ''')
+# Challenge 1
+# Compare Variables or Objects For Equivalence: 
+print('''
+Challenge # 1
+----------------------------------------------------
+Try changing the values of x and y and see what happends
+      
+Hint: https://www.w3schools.com/python/python_operators.asp
+----------------------------------------------------
+''')
+print('''Answer to Challenge 1
+-------------------------------------------------''')
+x=8  # <-- change the value of x
+y=4  # <-- change the value of y
+print("x = ", x)
+print("y = ", y)
+print("x == y \t\tis ", x == y)
+print("x != y \t\tis ", x != y)
+print("x > y \t\tis ", x > y)
+print("x < y \t\tis ", x < y)
+print("x >= y \t\tis ", x >= y)
+print("x <= y \t\tis ", x <= y)
+print("x is y \t\tis ", x is y)
+print("x is not y \tis ", x is not y)
+print('''-------------------------------------------------''')
